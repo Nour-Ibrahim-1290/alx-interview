c545d04febb7ca6fd93431c6b29b14c7e4c44177
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-28 07:30:23 +0300

    minimal operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Interview Practices"""
+
+
+def minOperations(n):
+    """
+        Your text editor can execute only two operations
+        in this file: Copy All and Paste.
+    """
+    if n <= 1:
+        return 0
+    num_operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            num_operations += divisor
+            n = n // divisor
+        divisor += 1
+    return num_operations

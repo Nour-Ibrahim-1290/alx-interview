f176adf2f21a260d8819028e72d74673dceac1d2
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-10-27 00:57:04 +0300

    updated-stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,7 @@
 
 import sys
 import signal
-
+import argparse
 
 def print_stats(status_codes, file_size):
     """
@@ -14,7 +14,6 @@ def print_stats(status_codes, file_size):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
 
-
 def signal_handler(sig, frame):
     """
         Handle the signal
@@ -22,15 +21,19 @@ def signal_handler(sig, frame):
     print_stats(status_codes, file_size)
     sys.exit(0)
 
-
 # Handle Request Status Codes
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 file_size = 0
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# Argument parser setup
+parser = argparse.ArgumentParser(description="Log Parsing")
+parser.add_argument('file', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="File to read log data from")
+args = parser.parse_args()
+
 try:
-    for i, line in enumerate(sys.stdin, 1):
+    for i, line in enumerate(args.file, 1):
         split_line = line.split()
         if len(split_line) < 2:
             continue

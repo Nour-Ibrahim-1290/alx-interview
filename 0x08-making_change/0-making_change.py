aef595d4caca232d2dfa879f92bca6032236a731
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-11-27 15:32:27 +0200

    makingChage-fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,52 +1,25 @@
 #!/usr/bin/python3
-"""Interview Mockups - MakeChange"""
-
-
-import heapq
-import numpy as np
+"""Change making module.
+"""
 
 
 def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
-        Given a pile of coins of different values,
-        determine the fewest number of coins needed
-        to meet a given amount total.
-    """
-    # if total <= 0:
-    #     return 0
-
-    # # Sort coins in descending order
-    # coins.sort(reverse=True)
-
-    # # Priority queue to keep track of the minimum number of coins
-    # pq = [(0, 0)]  # (number of coins, current total)
-    # visited = set()
-
-    # while pq:
-    #     num_coins, curr_total = heapq.heappop(pq)
-
-    #     if curr_total == total:
-    #         return num_coins
-
-    #     if curr_total > total or curr_total in visited:
-    #         continue
-
-    #     visited.add(curr_total)
-
-    #     for coin in coins:
-    #         heapq.heappush(pq, (num_coins + 1, curr_total + coin))
-
-    # return -1
     if total <= 0:
         return 0
-
-    # Initialize the dp array with a large value (total + 1)
-    dp = np.full(total + 1, total + 1, dtype=int)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return int(dp[total]) if dp[total] <= total else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
